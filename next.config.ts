4b74af1af52c11612a6153ea1c1a1f0e25dfd187
slantie <kandarp.gajjar.460@gmail.com>
2025-07-15 00:55:32 +0530

    Deployment Fixes - 3

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    allowedDevOrigins: ["http://localhost:3000"],
+    // allowedDevOrigins: ["http://localhost:3000"],
     reactStrictMode: true,
+    swcMinify: true,
 };
 
 export default nextConfig;
