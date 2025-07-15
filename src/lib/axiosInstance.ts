4338ee04a16b9a1aec1d255d73496fcdc67a48c8
slantie <kandarp.gajjar.460@gmail.com>
2025-07-15 13:08:45 +0530

    Deployment Fixes - 10

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -20,6 +20,9 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
     (config) => {
         const token = Cookies.get("authToken");
+        if (!config.headers) {
+            config.headers = {};
+        }
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
         }
